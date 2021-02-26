ea903a8b775d69d86881c178647348e2fdc90998
RitvarsE <ritvars.eglajs@outlook.com>
2021-02-26 23:38:32 +0200

    Edited to make it nested if, not foreach

--- basics/flow-of-control/05exercise.php
+++ basics/flow-of-control/05exercise.php
@@ -1,26 +1,29 @@
 <?php
 
-$keypad = [2 => 'ABC', 3 => 'DEF', 4 => 'GHI', 5 => 'JKL', 6 => 'MNO', 7 => 'PQRS', 8 => 'TUV', 9 => 'WXYZ'];
-$string = readline('Input your string: ');
+$string = strtoupper(readline('Input your string: '));
 
-function PhoneKeyPad(string $string, array $keypad): string
-{
-    $line = '';
+if (ctype_alpha($string)) {
 
-    if (ctype_alpha($string)) {
-
-        foreach (str_split($string) as $character) {
-
-            foreach ($keypad as $number => $characters) {
-
-                if (strlen(strpbrk($characters, strtoupper($character))) > 0) {
-                    $line .= str_repeat($number, (stripos($characters, $character) + 1));
-                }
-            }
+    foreach (str_split($string) as $character) {
+        if (strpbrk('ABC', $character)) {
+            echo str_repeat('2', strpos('ABC', $character) + 1);
+        } elseif (strpbrk('DEF', $character)) {
+            echo str_repeat('3', strpos('DEF', $character) + 1);
+        } elseif (strpbrk('GHI', $character)) {
+            echo str_repeat('4', strpos('GHI', $character) + 1);
+        } elseif (strpbrk('JKL', $character)) {
+            echo str_repeat('5', strpos('JKL', $character) + 1);
+        } elseif (strpbrk('MNO', $character)) {
+            echo str_repeat('6', strpos('MNO', $character) + 1);
+        } elseif (strpbrk('PQRS', $character)) {
+            echo str_repeat('7', strpos('PQRS', $character) + 1);
+        } elseif (strpbrk('TUV', $character)) {
+            echo str_repeat('8', strpos('TUV', $character) + 1);
+        } elseif (strpbrk('WXYZ', $character)) {
+            echo str_repeat('9', strpos('WXYZ', $character) + 1);
         }
-        return $line;
-    }
-    return 'Invalid input';
-}
 
-echo PhoneKeyPad($string, $keypad);
+    }
+} else {
+    echo 'Invalid input';
+}
