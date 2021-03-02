33f20089a257a3ca836937183ac9647f59c47195
RitvarsE <ritvars.eglajs@outlook.com>
2021-03-02 14:00:04 +0200

    added 04exercise

---
+++ basics/classes-and-objects/04exercise/Cinema.php
@@ -0,0 +1,24 @@
+<?php
+
+class Cinema{
+    private array $movies;
+
+
+    public function __construct(array $movies)
+    {
+        foreach ($movies as $movie) {
+            $this->addMovies($movie);
+        }
+    }
+    public function getMovies(): array
+    {
+        return $this->movies;
+    }
+
+    private function addMovies(Movie $movie): void
+    {
+        $this->movies[] = $movie;
+    }
+
+
+}
---
+++ basics/classes-and-objects/04exercise/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Movie.php';
+require_once  'Cinema.php';
+
+$cinema = new Cinema([
+    new Movie('Casino Royale', 'Eon Productions', 'PG13'),
+    new Movie('Glass', 'Buena Vista International', 'PG13'),
+    new Movie('Spider-Man: Into the Spider-Verse', 'Columbia Pictures', 'PG'),
+]);
+
+foreach ($cinema->getMovies() as $movie){
+    if($movie->getRating() === 'PG'){
+        echo $movie->getTitle(). ' ' . $movie->getStudio() . ' ' . $movie->getRating() . PHP_EOL;
+    }
+}
