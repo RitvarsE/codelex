c86a1ac57c8e3c591a5ab4ec7c18cc65f61141f8
RitvarsE <ritvars.eglajs@outlook.com>
2021-03-02 14:37:19 +0200

    added exercise 05

---
+++ basics/classes-and-objects/05exercise/Date.php
@@ -0,0 +1,56 @@
+<?php
+
+class Date
+{
+    private int $month;
+    private int $day;
+    private int $year;
+
+    public function __construct(int $month, int $day, int $year)
+    {
+        if ($month > 12 || $month < 1 || $day > 31 || $day < 1 || $year < 0) return;
+        $this->month = $month;
+        $this->day = $day;
+        $this->year = $year;
+    }
+
+
+    public function getMonth(): int
+    {
+        return $this->month;
+    }
+
+
+    public function setMonth(int $month): void
+    {
+        if ($month > 12 || $month < 1) return;
+        $this->month = $month;
+    }
+
+
+    public function getDay(): int
+    {
+        return $this->day;
+    }
+
+
+    public function setDay(int $day): void
+    {
+        if ($day > 31 || $day < 1) return;
+        $this->day = $day;
+    }
+
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+
+    public function setYear(int $year): void
+    {
+        if ($year < 0) return;
+        $this->year = $year;
+    }
+
+}
---
+++ basics/classes-and-objects/05exercise/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "Date.php";
+
+$birthday = new Date(15,9,1991); // 15 nav robežās, ja tālāk netaisīšu setMonth, tad būs errors.
+
+$birthday->setDay(50); // nenostrādās, jo nav robežās
+$birthday->setDay(15); // nostrādā
+
+$birthday->setMonth(15); // nenostrādā, jo nav robežās
+$birthday->setMonth(5); // nostrādā
+
+$birthday->setYear(-20); // nenostrādā
+$birthday->setYear(19999); // nostrādā
+
+echo $birthday->getDay() . '/' . $birthday->getMonth() . '/' . $birthday->getYear();
