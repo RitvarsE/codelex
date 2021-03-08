ca239e1beb804890e0eabf4d7d9221b0f3fc32b4
RitvarsE <ritvars.eglajs@outlook.com>
2021-03-08 12:22:22 +0200

    started flowershop

---
+++ tasks/flowerShop/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * FlowerShop
+ * List of flowers & prices
+ * Option to purchase
+ * First question: male/female
+ * if female -> apply 20% discount at end
+ * 3 different warehouses where flowers come from.
+ * FlowerShop -> Warehouse1/Warehouse2/Warehouse3
+ */
